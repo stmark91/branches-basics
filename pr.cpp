4d0735791d2be0b45152a988e6cfe2a72a023766
Марченко Александр Александрович <ip615s16@mwp09.csc.local>
2017-02-27 16:24:27 +0700

    Second commit

--- pr.cpp
+++ pr.cpp
@@ -4,9 +4,11 @@
 int main (){
 	const N=20;
 	int a[N], b, c;
-		for (i=0;i<N;i++){
-			a[i]=rand%200;
-		}
+	for (i=0;i<N;i++){
+	a[i]=rand%200;
+	}
+	for (i=0;i<N;i++){
+	cout << a[i];
 	return 0;
 }
 
